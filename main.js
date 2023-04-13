88d2d19438cd3e65fcc0481ece06d9f8f27197d9
George2497 <georgekoundouri@msn.com>
2023-04-13 13:05:02 +0100

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 'use strict';
 
 // Global Variables
+const inputBox = document.querySelector('.inputBox');
+const submitBtn = document.querySelector('.submitBtn');
+const tasks = document.querySelector('.tasks');
+
 const toDoItems = [];
 
-const enterTask = function () {
+submitBtn.addEventListener('click', function () {
+    console.log(`hello`);
+})
 
+const noTasks = function () {
+    if (!toDoItems) {
+        document.getElementsByClassName('.tasks').style.display = "none";
+    }
 }
