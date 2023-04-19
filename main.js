dfe3f6879173844ef8387627754cddd141038020
George Koundouri <georgekoundouri@msn.com>
2023-04-19 15:52:11 +0100

    Changes

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const inputBox = document.querySelector('.inputBox');
 const submitBtn = document.querySelector('.submitBtn');
 const tasks = document.querySelector('.tasks');
 
-
 // A blank array to hold onto the to-do items
 let toDoItems = JSON.parse(localStorage.getItem('toDoItems')) || [];
 console.log(toDoItems);
@@ -24,39 +23,36 @@ if (localStorage.getItem('toDoItems')) {
     }
 }
 
-// storedToDoItems();
 const addItem = function () {
     const inputBoxValue = inputBox.value.trim();
 
     // Checks to see if a value has been inputted
-    // if (inputBoxValue !== '') {
+    if (inputBoxValue !== '') {
 
-    // Create a new to-do item and add it to the array
-    const toDo = {
-        text: inputBoxValue,
-        completed: false
-    };
+        // Create a new to-do item and add it to the array
+        const toDo = {
+            text: inputBoxValue,
+            completed: false
+        };
 
-    // Push the to-do items to the toDoItems array to be posted
-    toDoItems.push(toDo);
+        // Push the to-do items to the toDoItems array to be posted
+        toDoItems.push(toDo);
 
-    settingLocalStorage();
+        settingLocalStorage();
 
-    // Add a new HTML item in the form of a list(li) when
-    // the "Submit" button has been pressed
-    const newToDoEl = document.createElement('li');
-    newToDoEl.innerText = inputBoxValue;
-    tasks.appendChild(newToDoEl);
-
-    // }
+        // Add a new HTML item in the form of a list(li) when
+        // the "Submit" button has been pressed
+        const newToDoEl = document.createElement('li');
+        newToDoEl.innerText = inputBoxValue;
+        tasks.appendChild(newToDoEl);
+    }
 
     // If the entry is empty do not post this to-do item
-    // if (inputBoxValue == '') {
-    //     return
-    // }
+    if (inputBoxValue == '') {
+        return
+    }
 }
 // Setting local storage for the to-do list
-
 submitBtn.addEventListener('click', addItem);
 
 const settingLocalStorage = function () {
